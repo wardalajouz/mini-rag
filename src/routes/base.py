0d6927b43cee7cda0b2933a18a8bc7cf7c759c53
wardalajouz <wardziad7@gmail.com>
2026-01-03 12:53:18 +0300

    Update: Added implementation for Video 8 (Multi-Query Retrieval)

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,8 +1,19 @@
-from fastapi import APIRouter
+from fastapi import FastAPI, APIRouter, Depends
+import os
+from helpers.config import get_settings, Settings
 
-# THIS MUST BE NAMED base_router
-base_router = APIRouter()
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1"],
+)
 
 @base_router.get("/")
-async def welcome():
-    return {"message": "Welcome to mini-RAG!"}
+async def welcome(app_settings: Settings = Depends(get_settings)):
+
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
+
+    return {
+        "app_name": app_name,
+        "app_version": app_version,
+    }
